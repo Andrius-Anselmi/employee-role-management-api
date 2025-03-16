8f1099959bcce730354bb690d02e528bdb72c997
andrius anselmi <andriusanselmi12@gmail.com>
2025-03-16 01:04:01 -0300

    feat: Adiciona Controller com rota GET

---
+++ src/main/java/dev/java10x/Gerenciamento/Controller.java
@@ -0,0 +1,15 @@
+package dev.java10x.Gerenciamento;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/bemvindo")
+    public String boasVindas() {
+        return "Seja bem vindo";
+    }
+}
