e95f1e324f9f5632af57d612efe2b2b999d70141
andrius anselmi <andriusanselmi12@gmail.com>
2025-03-23 13:02:43 -0300

    fix: altera o mapeamento do JpaRepository da entidade Funcionario

--- src/main/java/dev/java10x/Gerenciamento/Funcionario/FuncionarioRepository.java
+++ src/main/java/dev/java10x/Gerenciamento/Funcionario/FuncionarioRepository.java
@@ -3,5 +3,5 @@ package dev.java10x.Gerenciamento.Funcionario;
 import dev.java10x.Gerenciamento.Cargo.CargoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FuncionarioRepository extends JpaRepository<FuncionarioRepository, Long> {
+public interface FuncionarioRepository extends JpaRepository<FuncionarioModel, Long> {
 }
