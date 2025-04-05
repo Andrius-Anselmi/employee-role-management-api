2ddde9d4fc9830694b4e1220cdcc9a5af4a3dac6
andrius anselmi <andriusanselmi12@gmail.com>
2025-04-05 00:23:12 -0300

    feat: cria endpoints do Controlle de funcionario

--- src/main/java/dev/java10x/Gerenciamento/Funcionario/FuncionarioController.java
+++ src/main/java/dev/java10x/Gerenciamento/Funcionario/FuncionarioController.java
@@ -1,15 +1,45 @@
 package dev.java10x.Gerenciamento.Funcionario;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/funcionario")
 public class FuncionarioController {
 
     @GetMapping("/bemvindo")
     public String boasVindas() {
         return "Seja bem vindo";
+    }
+
+    //CADASTRAR FUNCIONARIO
+    @PostMapping("/cadastro")
+    public String cadastrarFuncionario() {
+
+        return "Cadastrando funcionario";
+    }
+    //EXIBIR FUNCIONARIOS
+    @GetMapping("/exibir")
+    public String exibirFuncionarios() {
+
+        return "Exibindo funcionarios";
+    }
+
+    //EXIBIR FUNCIONARIO POR ID
+    @GetMapping("/exibir{id}")
+    public String exibirPorId() {
+
+        return "Exibindo por ID";
+    }
+    //ALTERAR FUNCIONARIO POR ID
+    @PutMapping("/alterar/{id}")
+    public String alterarFuncionario() {
+
+        return "Alterando dados do funcionario";
+    }
+    //DELETAR FUNCIONARIO POR ID
+    @DeleteMapping("/deletar{id}")
+    public String deletarFuncionario() {
+
+        return "Deletando funcionario";
     }
 }
