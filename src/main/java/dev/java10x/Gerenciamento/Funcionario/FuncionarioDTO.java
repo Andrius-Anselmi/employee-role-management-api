faa3c61026ebaa519700af6d12a651d4b530dbaa
andrius anselmi <andriusanselmi12@gmail.com>
2025-04-10 11:12:17 -0300

    chore: Inclui a versão do DTO pela entidade Funcionario e mapeia manualmente no FuncionarioMapper os caminhos entidade x DTO

--- src/main/java/dev/java10x/Gerenciamento/Funcionario/FuncionarioDTO.java
+++ src/main/java/dev/java10x/Gerenciamento/Funcionario/FuncionarioDTO.java
@@ -1,4 +1,20 @@
 package dev.java10x.Gerenciamento.Funcionario;
+import dev.java10x.Gerenciamento.Cargo.CargoModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class FuncionarioDTO {
+
+        private Long id;
+        private String nome;
+        private int idade;
+        private String cidade;
+        private String uf;
+        private CargoModel cargos;
+
 }
