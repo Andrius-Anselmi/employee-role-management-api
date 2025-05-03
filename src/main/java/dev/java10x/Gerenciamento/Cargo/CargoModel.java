16dd3fb6246c712464d0a5a03c66338665a27a8d
andrius anselmi <andriusanselmi12@gmail.com>
2025-05-02 21:15:42 -0300

    refactor: Inclui a anottation toString nos models de Funcionario e Cargo

--- src/main/java/dev/java10x/Gerenciamento/Cargo/CargoModel.java
+++ src/main/java/dev/java10x/Gerenciamento/Cargo/CargoModel.java
@@ -6,12 +6,14 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Entity
+@ToString
 @Table(name = "tb_cargos")
 public class CargoModel {
 
--- src/main/java/dev/java10x/Gerenciamento/Funcionario/FuncionarioModel.java
+++ src/main/java/dev/java10x/Gerenciamento/Funcionario/FuncionarioModel.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ import java.util.List;
 @AllArgsConstructor
 @Data
 @Entity
+@ToString(exclude = "cargos")
 @Table(name = "tb_funcionario")
 public class FuncionarioModel {
 
