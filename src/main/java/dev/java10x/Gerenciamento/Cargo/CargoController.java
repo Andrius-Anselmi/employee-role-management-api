78be7f1c299e314fdac663d7e825f50318827e07
andrius anselmi <andriusanselmi12@gmail.com>
2025-05-03 00:11:02 -0300

    Implementacao de metodos CRUD(cadastrar,deletar,exibir) no controller de Cargo

--- src/main/java/dev/java10x/Gerenciamento/Cargo/CargoController.java
+++ src/main/java/dev/java10x/Gerenciamento/Cargo/CargoController.java
@@ -2,33 +2,39 @@ package dev.java10x.Gerenciamento.Cargo;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/cargo")
 
 public class CargoController {
 
+    private final CargoService cargoService;
+
+    public CargoController(CargoService cargoService) {
+        this.cargoService = cargoService;
+    }
+
     //CRIAR CARGO
     @PostMapping("cadastro")
-    public String cadastrarCargo(){
-
-        return "cargo criado";
+    public CargoModel cadastrarCargo(@RequestBody CargoModel cargoCriado){
+        return cargoService.cadastrarCargo(cargoCriado);
     }
     //EXIBIR CARGO
     @GetMapping("exibir")
-    public String exibirCargos(){
-
-        return "exibindo cargos";
+    public List<CargoModel> exibirCargos(){
+        return  cargoService.exibirCargos();
     }
     //EXIBIR CARGO POR ID
     @GetMapping("exibir/{id}")
-    public String exibirPorId(){
-        return "exibindo cargo de id";
+    public CargoModel exibirPorId(@PathVariable Long id){
+        return cargoService.exibirPorId(id);
     }
 
     //DELETAR CARGO POR ID
     @DeleteMapping("deletar/{id}")
-    public String deletar(){
-        return "deletando cargo x";
+    public void deletar(@PathVariable Long id){
+        cargoService.deletar(id);
     }
 
     //ALTERAR CARGO POR ID
