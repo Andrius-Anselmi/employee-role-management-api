a897b1bc13c264d5f2682157d692d11a49370b87
andrius anselmi <andriusanselmi12@gmail.com>
2025-05-02 22:22:58 -0300

    feat: criar endpoints básicos (GET, POST, PUT, DELETE) no controller de cargos

--- src/main/java/dev/java10x/Gerenciamento/Cargo/CargoController.java
+++ src/main/java/dev/java10x/Gerenciamento/Cargo/CargoController.java
@@ -1,4 +1,41 @@
 package dev.java10x.Gerenciamento.Cargo;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cargo")
+
 public class CargoController {
+
+    //CRIAR CARGO
+    @PostMapping("cadastro")
+    public String cadastrarCargo(){
+
+        return "cargo criado";
+    }
+    //EXIBIR CARGO
+    @GetMapping("exibir")
+    public String exibirCargos(){
+
+        return "exibindo cargos";
+    }
+    //EXIBIR CARGO POR ID
+    @GetMapping("exibir/{id}")
+    public String exibirPorId(){
+        return "exibindo cargo de id";
+    }
+
+    //DELETAR CARGO POR ID
+    @DeleteMapping("deletar/{id}")
+    public String deletar(){
+        return "deletando cargo x";
+    }
+
+    //ALTERAR CARGO POR ID
+    @PutMapping("alterar/{id}")
+    public String alterarCargo(){
+        return "alterando cargo de id";
+    }
+
+
 }
