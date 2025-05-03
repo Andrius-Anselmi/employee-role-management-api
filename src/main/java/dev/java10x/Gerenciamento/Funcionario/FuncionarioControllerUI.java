5722190703ab0c187827665b25a69f3e29e612f5
andrius anselmi <andriusanselmi12@gmail.com>
2025-05-02 22:06:40 -0300

    feat: implementar controller de entrada para renderização de dados

---
+++ src/main/java/dev/java10x/Gerenciamento/Funcionario/FuncionarioControllerUI.java
@@ -0,0 +1,28 @@
+package dev.java10x.Gerenciamento.Funcionario;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("funcionario/ui")
+
+public class FuncionarioControllerUI {
+
+    private final FuncionarioService funcionarioService;
+
+    public FuncionarioControllerUI(FuncionarioService funcionarioService) {
+        this.funcionarioService = funcionarioService;
+    }
+
+    @GetMapping("listar")
+    public String listarNinjas(Model model) {
+        List<FuncionarioDTO> funcionarios = funcionarioService.exibirFuncionarios();
+        model.addAttribute("funcionarios", funcionarios);
+        return "funcionarios";
+    }
+}
