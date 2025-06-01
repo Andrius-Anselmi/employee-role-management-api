a2eb3cbed9ec42a391470d8e0c310e5e1882c6ee
andrius <andriusanselmi12@gmail.com>
2025-05-31 22:01:42 -0300

    "fix: Corrigir exibição de dados no service de Cargo"

--- src/main/java/dev/java10x/Gerenciamento/Cargo/CargoService.java
+++ src/main/java/dev/java10x/Gerenciamento/Cargo/CargoService.java
@@ -13,12 +13,8 @@ import java.util.stream.Collectors;
 @Service
 public class CargoService {
 
-    private final CargoRepository cargoRepository;
+    private CargoRepository cargoRepository;
     private CargoMapper cargoMapper;
-
-    public CargoService(CargoRepository cargoRepository) {
-        this.cargoRepository = cargoRepository;
-    }
 
     public CargoService(CargoRepository cargoRepository, CargoMapper cargoMapper) {
         this.cargoRepository = cargoRepository;
@@ -27,23 +23,22 @@ public class CargoService {
 
     //CRIAR CARGO
     @PostMapping("cadastro")
-    public CargoDTO cadastrarCargo(CargoDTO cargoDTO){
-        CargoModel cargoSalvo = cargoMapper.map(cargoDTO);
+    public CargoDetalhadoDTO cadastrarCargo(CargoDetalhadoDTO cargoDetalhadoDTO){
+        CargoModel cargoSalvo = cargoMapper.map(cargoDetalhadoDTO);
         cargoSalvo = cargoRepository.save(cargoSalvo);
         return cargoMapper.map(cargoSalvo);
     }
 
     //EXIBIR CARGOS
     @GetMapping("exibir")
-    public List<CargoDTO> exibirCargos(){
+    public List<CargoResumoDTO> exibirCargos(){
         List<CargoModel> listaDeCargos  = cargoRepository.findAll();
-        return listaDeCargos.stream()
-                .map(cargoMapper::map).collect(Collectors.toList());
+        return listaDeCargos.stream().map(cargoModel -> cargoMapper.mapResumo(cargoModel)).toList();
     }
 
     //EXIBIR CARGO POR ID
     @GetMapping("exibir/{id}")
-    public CargoDTO exibirPorId(Long id){
+    public CargoDetalhadoDTO exibirPorId(Long id){
         Optional<CargoModel> cargoBuscado = cargoRepository.findById(id);
         return cargoBuscado.map(cargoMapper::map).orElse(null);
     }
@@ -56,13 +51,13 @@ public class CargoService {
 
     //ALTERAR CARGO POR ID
     @PutMapping("alterar/{id}")
-    public CargoDTO alterarCargo(Long id, CargoDTO cargo){
+    public CargoDetalhadoDTO alterarCargo(Long id, CargoDetalhadoDTO cargo){
         Optional<CargoModel> cargoExistente = cargoRepository.findById(id);
         if(cargoExistente.isPresent()){
             CargoModel cargoAtualizado = cargoMapper.map(cargo);
             cargo.setId(id);
             CargoModel cargoSalvo = cargoRepository.save(cargoAtualizado);
-            return  cargoMapper.map(cargoSalvo);
+            return cargoMapper.map(cargoSalvo);
         }
 
         return null;
