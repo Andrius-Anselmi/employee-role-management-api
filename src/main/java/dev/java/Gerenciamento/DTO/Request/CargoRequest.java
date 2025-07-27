ce2350379f0c2ca3d9006e95c4c96ac087ead245
Andrius Anselmi <andriusanselmi12@gmail.com>
2025-07-27 16:29:53 -0300

    refactor: Tranforma CargoRequest em record, e exclui o ModelMapper

---
+++ src/main/java/dev/java/Gerenciamento/DTO/Request/CargoRequest.java
@@ -0,0 +1,10 @@
+package dev.java.Gerenciamento.DTO.Request;
+
+import java.math.BigDecimal;
+
+public record CargoRequest(String nome, String descricao, BigDecimal salario, String nivel){
+}
+
+
+
+
