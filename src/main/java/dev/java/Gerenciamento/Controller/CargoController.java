27315112578d36750fb2bb5ac524bb5efa27e994
Andrius Anselmi <andriusanselmi12@gmail.com>
2025-07-27 16:38:01 -0300

    refactor: Refatora o controller da entidade e implementa o uso do ResponseEntity nos endpoints

--- src/main/java/dev/java/Gerenciamento/Controller/CargoController.java
+++ src/main/java/dev/java/Gerenciamento/Controller/CargoController.java
@@ -1,53 +1,58 @@
 package dev.java.Gerenciamento.Controller;
-import dev.java.Gerenciamento.entity.CargoModel;
+import dev.java.Gerenciamento.DTO.Request.CargoRequest;
+import dev.java.Gerenciamento.DTO.Response.CargoResponse;
+import dev.java.Gerenciamento.Mapper.CargoMapper;
 import dev.java.Gerenciamento.Service.CargoService;
+import dev.java.Gerenciamento.entity.Cargo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/cargo")
-
+@RequiredArgsConstructor
 public class CargoController {
 
     private final CargoService cargoService;
 
-    public CargoController(CargoService cargoService) {
-        this.cargoService = cargoService;
-    }
-
 
     @PostMapping()
-    public CargoModel cadastrarCargo(@RequestBody CargoModel cargoCriado) {
-        return cargoService.cadastrarCargo(cargoCriado);
+    public ResponseEntity<CargoResponse> salvar(@RequestBody CargoRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).
+                body(CargoMapper.toResponse(cargoService.salvar(CargoMapper.toCargo(request))));
     }
-
 
     @GetMapping()
-    public List<CargoModel> exibirCargos() {
-        return cargoService.exibirCargos();
+    public ResponseEntity<List<CargoResponse>> listarTodos() {
+        return ResponseEntity.ok(cargoService.exibirCargos().stream().map(CargoMapper::toResponse).toList());
     }
 
-
     @GetMapping("{id}")
-    public CargoModel exibirPorId(@PathVariable Long id) {
-        if (cargoService.exibirPorId(id) != null) {
-            return cargoService.exibirPorId(id);
-        } else
-            return null;
+    public ResponseEntity<CargoResponse> buscarPorId(@PathVariable Long id) {
+        Optional<Cargo> cargoOpt = cargoService.exibirPorId(id);
+        return cargoOpt.map(cargo -> ResponseEntity.ok(CargoMapper.toResponse(cargo))).
+                orElse(ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("{id}")
-    public void deletar(@PathVariable Long id) {
-        cargoService.deletar(id);
+    public ResponseEntity<Void> excluir(@PathVariable Long id) {
+
+        if(cargoService.exibirPorId(id).isPresent()) {
+            cargoService.deletar(id);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.noContent().build();
     }
 
 
     @PutMapping("{id}")
-    public CargoModel alterarCargo(@RequestBody CargoModel cargo, @PathVariable Long id) {
-        if(cargoService.exibirPorId(id) != null){
-            cargoService.alterarCargo(id,cargo);
-        }
-        return null;
+    public ResponseEntity<CargoResponse> alterarCargo(@RequestBody CargoRequest request, @PathVariable Long id) {
+            return cargoService.alterarCargo(id,CargoMapper.toCargo(request)).map
+                    ( cargo -> ResponseEntity.ok(CargoMapper.toResponse(cargo))).orElse(ResponseEntity.notFound().build());
 }
         }
 
