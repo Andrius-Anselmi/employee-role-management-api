0c451efba22f99841d4a3c75440ca07cf66f9f96
Andrius Anselmi <andriusanselmi12@gmail.com>
2025-07-28 18:21:26 -0300

    feat: Adiciona o método de alterar funcionário

--- src/main/java/dev/java/Gerenciamento/Controller/FuncionarioController.java
+++ src/main/java/dev/java/Gerenciamento/Controller/FuncionarioController.java
@@ -1,11 +1,13 @@
 package dev.java.Gerenciamento.Controller;
 
-import dev.java.Gerenciamento.Controller.Request.FuncionarioRequest;
-import dev.java.Gerenciamento.Controller.Response.FuncionarioResponse;
+import dev.java.Gerenciamento.DTO.Request.FuncionarioRequest;
+import dev.java.Gerenciamento.DTO.Response.FuncionarioResponse;
 import dev.java.Gerenciamento.Mapper.FuncionarioMapper;
-import dev.java.Gerenciamento.entity.Funcionario;
 import dev.java.Gerenciamento.Service.FuncionarioService;
+import dev.java.Gerenciamento.entity.Funcionario;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,30 +21,42 @@ public class FuncionarioController {
     private final FuncionarioService funcionarioService;
 
     @PostMapping()
-    public FuncionarioResponse criarFuncionario(@RequestBody FuncionarioRequest request) {
-        Funcionario funcionario = FuncionarioMapper.toModel(request);
-        Funcionario funcionarioSalvo = funcionarioService.criarFuncionario(funcionario);
-        return FuncionarioMapper.toResponse(funcionarioSalvo);
+    public ResponseEntity<FuncionarioResponse> salvar(@RequestBody FuncionarioRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).
+                body(FuncionarioMapper.toResponse(funcionarioService.salvar(FuncionarioMapper.toFuncionario(request))));
     }
 
     @GetMapping()
-    public List<FuncionarioResponse> ListarFuncionarios() {
-        List<Funcionario> funcionarios = funcionarioService.exibirFuncionarios();
-        return funcionarios.stream().map(FuncionarioMapper::toResponse).toList();
+    public ResponseEntity<List<FuncionarioResponse>> listar() {
+        return ResponseEntity.ok(funcionarioService.listarFuncionarios().
+                stream().map(FuncionarioMapper::toResponse).toList());
     }
 
-    @GetMapping("{id}")
-    public FuncionarioResponse ListarPorId(@PathVariable Long id) {
-        Optional<Funcionario> funcionarioOpt = funcionarioService.exibirFuncionariosPorId(id);
-        return FuncionarioMapper.toResponse(funcionarioOpt.orElse(null));
+    @GetMapping("/{id}")
+    public ResponseEntity<FuncionarioResponse> bucarPorId(@PathVariable Long id) {
+        return funcionarioService.buscarFuncionarioPorId(id).map
+                        (funcionario -> ResponseEntity.ok(FuncionarioMapper.toResponse(funcionario)))
+                .orElse(ResponseEntity.notFound().build());
     }
 
-    @DeleteMapping("{id}")
-    public void deletar(@PathVariable Long id) {
-        funcionarioService.deletarFuncionarioPorId(id);
+    @PutMapping("/{id}")
+    public ResponseEntity<FuncionarioResponse> alterar(@PathVariable Long id, FuncionarioRequest request) {
+        return funcionarioService.alterarFuncionario(id, FuncionarioMapper.toFuncionario(request))
+                .map(funcionario -> ResponseEntity.ok(FuncionarioMapper.toResponse(funcionario)))
+                .orElse(ResponseEntity.notFound().build());
     }
 
-
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+        Optional<Funcionario> Optfuncionario = funcionarioService.buscarFuncionarioPorId(id);
+        if (Optfuncionario.isPresent()) {
+            funcionarioService.deletarFuncionario(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
+
+
 
 
