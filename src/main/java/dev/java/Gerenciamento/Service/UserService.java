fb4f606b692cbb4de5564d32e03fef2515d11ebe
Andrius Anselmi <andriusanselmi12@gmail.com>
2025-08-03 15:05:37 -0300

    feat: Adicona o método PasswordEncoder para encriptografar a senha do usuário

--- src/main/java/dev/java/Gerenciamento/Service/UserService.java
+++ src/main/java/dev/java/Gerenciamento/Service/UserService.java
@@ -3,15 +3,20 @@ package dev.java.Gerenciamento.Service;
 import dev.java.Gerenciamento.Repository.UserRepository;
 import dev.java.Gerenciamento.entity.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
+    private final PasswordEncoder encoder;
+
     private final UserRepository userRepository;
 
     public User registrar(User user){
+        String senha = user.getSenha();
+        user.setSenha(encoder.encode(senha));
         return userRepository.save(user);
     }
 
